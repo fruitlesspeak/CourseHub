66772df5d433c30143c0440edda2ab4a6c6ef307
Opeyemi <ogundimo@myumanitooba.ca>
2026-02-05 01:25:31 -0600

    Update App.spec.ts to test router-view rendering

--- frontend/src/__tests__/App.spec.ts
+++ frontend/src/__tests__/App.spec.ts
@@ -1,11 +1,14 @@
 import { describe, it, expect } from 'vitest'
-
 import { mount } from '@vue/test-utils'
 import App from '../App.vue'
 
 describe('App', () => {
-  it('mounts renders properly', () => {
-    const wrapper = mount(App)
-    expect(wrapper.text()).toContain('You did it!')
+  it('mounts and renders router-view', () => {
+    const wrapper = mount(App, {
+      global: {
+        stubs: ['router-view']
+      }
+    })
+    expect(wrapper.findComponent({ name: 'router-view' }).exists()).toBe(true)
   })
 })
